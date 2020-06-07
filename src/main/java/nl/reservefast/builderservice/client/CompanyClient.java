154ee44efcae0b824dddf44511dab5fd2e3369fe
Rens Manders <info@rensmanders.nl>
2020-06-07 22:29:18 +0200

    heroku

--- src/main/java/nl/reservefast/builderservice/client/CompanyClient.java
+++ src/main/java/nl/reservefast/builderservice/client/CompanyClient.java
@@ -1,12 +1,13 @@
 package nl.reservefast.builderservice.client;
 
 import nl.reservefast.builderservice.model.Company;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("company-service")
-
+@RibbonClient("company-service")
 public interface CompanyClient {
 
     @GetMapping("/{id}")
