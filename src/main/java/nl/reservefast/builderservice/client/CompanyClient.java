4664f4e76fa05264771d13a1b9ab06b723996d51
Rens Manders <info@rensmanders.nl>
2020-06-07 23:07:01 +0200

    heroku

--- src/main/java/nl/reservefast/builderservice/client/CompanyClient.java
+++ src/main/java/nl/reservefast/builderservice/client/CompanyClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("company-service")
+@FeignClient(name = "company-service", url = "https://reservefast-company.herokuapp.com")
 public interface CompanyClient {
 
     @GetMapping("/{id}")
